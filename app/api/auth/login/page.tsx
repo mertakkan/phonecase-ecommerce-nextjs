06332487531fdf08b13b1dc2c54d0219245128c3
Muzaffer Mert Akkan <makkan19@ku.edu.tr>
2024-05-06 18:42:41 +0300

    LoginPage update

--- app/api/auth/login/page.tsx
+++ app/api/auth/login/page.tsx
@@ -5,13 +5,31 @@ import { Label } from '@/components/ui/label';
 
 export default function LoginPage() {
   return (
-    <form>
-      <Label htmlFor="email">Email:</Label>
-      <Input id="email" name="email" type="email" required />
-      <Label htmlFor="password">Password:</Label>
-      <Input id="password" name="password" type="password" required />
-      <Button formAction={login}>Log in</Button>
-      <Button formAction={signup}>Sign up</Button>
-    </form>
+    <div className="flex justify-center items-center h-screen">
+      <div className="p-10 shadow-md">
+        <div className="flex flex-col w-96 justify-center">
+          <form className="space-y-4">
+            <div>
+              <Label htmlFor="email">Email:</Label>
+              <Input
+                placeholder="johndoe@mail.com"
+                id="email"
+                name="email"
+                type="email"
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password:</Label>
+              <Input id="password" name="password" type="password" required />
+            </div>
+            <div className="flex space-x-6">
+              <Button formAction={login}>Log in</Button>
+              <Button formAction={signup}>Sign up</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
