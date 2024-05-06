8c3dca9918d8d3a04e7e4043fd5a947bf7eb0571
Muzaffer Mert Akkan <makkan19@ku.edu.tr>
2024-05-07 00:15:40 +0300

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 import Providers from '@/components/providers';
 import { constructMetadata } from '@/lib/utils';
 
-export const metadata: Metadata = {
-  title: 'CaseCobra',
-  description: 'E-Commerce Case Cobra',
-};
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
@@ -22,10 +19,14 @@ export default function RootLayout({
       <body className={fonts.recursive}>
         <Navbar />
 
-        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
-          <div></div>
-          {children}
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <Providers>{children}</Providers>
+          </div>
+          <Footer />
         </main>
+
+        <Toaster />
       </body>
     </html>
   );
